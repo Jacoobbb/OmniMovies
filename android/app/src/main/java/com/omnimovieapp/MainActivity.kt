437aa17d4378ebdc6555bb28d5b1e08860942bae
Jacoobbb <wil.jacob1997@gmail.com>
2025-02-09 13:51:25 +0800

    feat: install native stack navigation

--- android/app/src/main/java/com/omnimovieapp/MainActivity.kt
+++ android/app/src/main/java/com/omnimovieapp/MainActivity.kt
@@ -13,6 +13,11 @@ class MainActivity : ReactActivity() {
    */
   override fun getMainComponentName(): String = "omnimovieapp"
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+    ReactFontManager.getInstance().addCustomFont(this, "Poppins", R.font.poppins)
+  }
+
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
