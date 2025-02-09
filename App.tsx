b093dbbc71f4be7862ed3c70501610cc6498110b
Jacoobbb <wil.jacob1997@gmail.com>
2025-02-09 12:41:32 +0800

    refactor: downgrade react native

--- App.tsx
+++ App.tsx
@@ -1,3 +1,36 @@
-export default function App() {
-  return <></>
+
+import Header from "./src/common/components/Header";
+import { Platform, StatusBar, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+if (Platform.OS === "android") {
+  StatusBar.setTranslucent(true);
+  StatusBar.setBackgroundColor("transparent");
 }
+StatusBar.setBarStyle("light-content");
+
+export default function App() {
+
+
+  const leftElement = (<Text style={{fontSize: 20, fontWeight: 'bold', color: '#ffffff'}}>Film List</Text>);
+  // const rightElement = (<Text style={{fontSize: 20, fontWeight: 'bold', color: '#ffffff'}}>Film List</Text>)
+
+  return (
+      <SafeAreaProvider>
+        <SafeAreaView 
+          style={styles.root} 
+
+        >
+                <Header leftElement={leftElement} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+
+
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1, backgroundColor: '#000000'
+  }
+});
---
+++ common/components/Header.tsx
@@ -0,0 +1,54 @@
+import { Dimensions, Image, Pressable, StatusBar, StyleSheet, Text, View } from "react-native";
+import { BACKGROUND_IMAGE, STATUSBAR_HEIGHT } from "../constants";
+
+interface HeaderProps {
+    leftElement: JSX.Element;
+    leftAction?: () => void;
+    rightElement?: JSX.Element;
+    rightAction?: () => void;
+}
+
+export default function Header(props: HeaderProps) {
+
+    const {leftElement, leftAction, rightElement, rightAction} = props;
+
+    return (
+        <View>
+            <View style={styles.headerContainer}>
+                <Pressable style={styles.headerLeft} onPress={leftAction}>
+                    {leftElement}
+                </Pressable>
+                <Pressable style={styles.headerRight} onPress={rightAction}>
+                    {rightElement}
+                </Pressable>
+            </View>
+            <Image style={styles.background} source={BACKGROUND_IMAGE} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        zIndex: 2, 
+        height: 70, 
+        padding: 20, 
+        flexDirection: 'row', 
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+    headerLeft: {
+        flex: 1
+    },
+    headerRight: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'flex-end'
+    },
+    background: {
+        flex: 1, 
+        position: 'absolute', 
+        width: '100%', 
+        height: Dimensions.get('window').height * 0.6, 
+        marginTop: (-70 - STATUSBAR_HEIGHT)
+    }
+})
